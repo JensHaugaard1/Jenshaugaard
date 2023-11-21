96746a7c948440431374c3996ce7bba231401ae7
JensHaugaard1 <136031062+JensHaugaard1@users.noreply.github.com>
2023-11-21 23:32:17 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,44 +1,5 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let mousePosX = 0,
-        mousePosY = 0,
-        mouseCircle = document.getElementById('mouse-circle');
-
-    document.onmousemove = (e) => {
-        mousePosX = e.pageX;
-        mousePosY = e.pageY;
-    }
-
-    let delay = 6,
-        revisedMousePosX = 0,
-        revisedMousePosY = 0;
-
-    function delayMouseFollow() {
-        requestAnimationFrame(delayMouseFollow);
-
-        revisedMousePosX += (mousePosX - revisedMousePosX) / delay;
-        revisedMousePosY += (mousePosY - revisedMousePosY) / delay; 
-
-        mouseCircle.style.top = revisedMousePosY + 'px';
-        mouseCircle.style.left = revisedMousePosX + 'px';
-    }
-    delayMouseFollow();
-
-
+var cursor = document.getElementById("cursor");
+document.body.addEventListener("mousemove", function(e) {
+  cursor.style.left = e.clientX + "px",
+    cursor.style.top = e.clientY + "px";
 });
-
-function startTime() {
-    const today = new Date();
-    let h = today.getHours();
-    let m = today.getMinutes();
-    let s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
-    setTimeout(startTime, 1000);
-  }
-  
-  function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-  }
-
