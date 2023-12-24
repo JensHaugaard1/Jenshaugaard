1777ff8694449de2db043dade047742a1776e22f
JensHaugaard1 <136031062+JensHaugaard1@users.noreply.github.com>
2023-12-24 17:25:44 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ document.body.addEventListener("mousemove", function(e) {
   cursor.style.left = e.clientX + "px",
     cursor.style.top = e.clientY + "px";
 });
+$(window).scroll(function(){
+    if ($(window).scrollTop() == $(document).height()-$(window).height()){
+       $.ajax({
+          url: "index.html",
+          success: function (data) { $('body').append(data); },
+          dataType: 'html'
+       });
+    }
+});
